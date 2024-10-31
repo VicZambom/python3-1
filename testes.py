284d7747c70f0897ee06f706980557b6e46ae0eb
VicZambom <140662019+VicZambom@users.noreply.github.com>
2024-10-31 19:03:16 -0300

    hello

---
+++ testes.py
@@ -0,0 +1,12 @@
+my_name = "Victoria"
+print("Hello and welcome " + my_name + "!")
+
+#coments___________________________
+#this variable will be used to count the number of times anyone tweets the world persnickety
+persnickety_count = 0
+
+#This code will calculate the likelihood that ir will rain tomorrow
+complicated_rain_calculation_for_tomorrow()
+
+#useful_value = old_sloppy_code()
+useful_value = new_clean_code()
